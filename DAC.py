01f872bb6e8a33260990dda296b8eac0f4896d02
jackberger03 <136964884+jackberger03@users.noreply.github.com>
2024-08-30 15:52:30 -0500

    raw

--- DAC.py
+++ DAC.py
@@ -13,7 +13,7 @@ def sin_wave():
 
     while True:
         voltage = 2048 * (1.0 + 0.5 * math.sin(6.2832 * t))
-        dac.set_voltage(int(voltage))
+        dac.raw_value = int(voltage)
         t += tStep
         time.sleep(0.0005)
 
